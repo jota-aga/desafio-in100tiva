5a3f04444b5053d316daa15fd14d9d1b9d04d4f5
joão henrique <joaolgg2estudos@gmail.com>
2026-05-12 15:35:23 -0300

    movendo classe para um pacote melhor especificado e adicionando testes

--- src/test/java/com/in100tiva/jh/todolist/repository/TarefaRepositoryTest.java
+++ src/test/java/com/in100tiva/jh/todolist/repository/TarefaRepositoryTest.java
@@ -1,5 +1,6 @@
-package com.in100tiva.jh.todolist;
+package com.in100tiva.jh.todolist.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.List;
@@ -29,14 +30,39 @@ public class TarefaRepositoryTest {
 				.descricao("descrição")
 				.status(StatusDaTarefa.PENDENTE)
 				.build();
+		tarefaRepository.deleteAll();
 	}
 	
 	@Test
 	public void shouldSaveSucessfully() {
 		tarefaRepository.save(tarefa);
 		
-		List<Tarefa> tarefas = tarefaRepository.findAllByTituloContains(tarefa.getTitulo());
+		List<Tarefa> tarefas = tarefaRepository.findAll();
 		
 		assertFalse(tarefas.isEmpty());
+	}
+	
+	@Test
+	public void shouldFindOnlyByStatus() {
+		Tarefa tarefa2 = Tarefa.builder()
+				.titulo("titulo")
+				.descricao("descrição")
+				.status(StatusDaTarefa.FINALIZADA)
+				.build();
+		tarefaRepository.saveAll(List.of(tarefa, tarefa2));
+		
+		List<Tarefa> tarefas = tarefaRepository.findAllByStatus(StatusDaTarefa.FINALIZADA);
+		
+		assertEquals(tarefas.size(), 1);
+	}
+	
+	@Test
+	public void shouldFindByTituloContain() {
+		tarefaRepository.save(tarefa);
+
+		
+		List<Tarefa> tarefas = tarefaRepository.findAllByTituloContains("tit");
+		
+		assertEquals(tarefas.size(), 1);
 	}
 }
